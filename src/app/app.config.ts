c97e05174942f22f3598ee36a9bc925b53d365cb
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-11-26 04:54:35 -0500

    Add Firebase configuration and update environment settings
    - Create firebase.json for Firebase setup
    - Update .gitignore to exclude Firebase-related files
    - Add environment.ts for environment variables
    - Modify angular.json for file replacements in development
    - Update package.json to include Firebase and AngularFire dependencies
    - Configure app.module.ts to initialize Firebase services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": environment.projectId,
+      "appId": environment.appId,
+      "storageBucket": environment.storageBucket,
+      "apiKey": environment.apiKey,
+      "authDomain": environment.authDomain,
+      "messagingSenderId": environment.messagingSenderId
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  projectId: 'PROJECT_ID',
+  appId: 'APP_ID',
+  storageBucket: 'STORAGE_BUCKET',
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN',
+  messagingSenderId: 'MESSAGING_SENDER_ID'
+};
