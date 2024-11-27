c133977e8c8f2be424a0d26f1cb0d65d946065fc
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-11-26 19:08:46 -0500

    Add Font Awesome icons to dashboard component and update dependencies

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faAngry } from '@fortawesome/free-regular-svg-icons';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
@@ -11,6 +14,11 @@ export class DashboardComponent implements OnInit {
     usersCount: 120,
     ordersCount: 45,
     revenue: 8000,
+  };
+
+  icons: { [key: string]: IconDefinition; } = {
+    angry: faAngry,
+    paw: faPaw,
   };
 
   constructor() {}
