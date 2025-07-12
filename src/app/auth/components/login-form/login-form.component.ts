60a3ea6889d9eeeb3457c5f164da8218576629a4
Jaime Meza <jaimealbertomeza@hotmail.com>
2025-07-12 15:27:13 -0500

    feat: update password validation to require a minimum length of 8 characters

--- src/app/auth/components/login-form/login-form.component.ts
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -33,7 +33,7 @@ export class LoginFormComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
       Validators.required,
-      Validators.minLength(6),
+      Validators.minLength(8),
     ]),
   });
 
