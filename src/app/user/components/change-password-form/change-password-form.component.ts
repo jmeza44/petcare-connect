b43cd230ec1ec5e3fac553c33fad59ea2991250f
Jaime Meza <jaimealbertomeza@hotmail.com>
2025-07-12 16:37:44 -0500

    feat: refactor change password form to use custom form password component and improve validation handling

--- src/app/user/components/change-password-form/change-password-form.component.ts
+++ src/app/user/components/change-password-form/change-password-form.component.ts
@@ -4,6 +4,7 @@ import {
   AbstractControl,
   AbstractControlOptions,
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   ValidationErrors,
@@ -11,55 +12,63 @@ import {
 } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { ChangePasswordRequest } from '../../models';
+import { FormPasswordComponent } from '../../../shared/components/inputs/form-password/form-password.component';
+import { getFormControlAndState } from '../../../shared/utils/form-control.utils';
 
 @Component({
   selector: 'pet-change-password-form',
-  imports: [ReactiveFormsModule, ButtonComponent],
+  imports: [ReactiveFormsModule, FormPasswordComponent, ButtonComponent],
   templateUrl: './change-password-form.component.html',
 })
 export class ChangePasswordFormComponent {
   @Input() isLoading = false;
   @Output() submitted = new EventEmitter<ChangePasswordRequest>();
 
-  form: FormGroup;
-  errorMessage: string = '';
+  form = new FormGroup(
+    {
+      currentPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/[A-Z]/),
+        Validators.pattern(/[a-z]/),
+        Validators.pattern(/[0-9]/),
+        Validators.pattern(/[^a-zA-Z0-9]/),
+      ]),
+      confirmPassword: new FormControl('', Validators.required),
+    },
+    {
+      validators: [this.passwordsMustDiffer.bind(this)],
+    } as AbstractControlOptions,
+  );
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group(
-      {
-        currentPassword: ['', Validators.required],
-        newPassword: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(8),
-            Validators.pattern(/[A-Z]/),
-            Validators.pattern(/[a-z]/),
-            Validators.pattern(/[0-9]/),
-            Validators.pattern(/[^a-zA-Z0-9]/),
-          ],
-        ],
-        confirmPassword: ['', Validators.required],
-      },
-      {
-        validators: [this.passwordsMustDiffer.bind(this)],
-      } as AbstractControlOptions,
-    );
+  constructor() {}
+
+  get currentPasswordControl() {
+    return getFormControlAndState(this.form, 'currentPassword');
   }
 
-  get f() {
-    return this.form.controls;
+  get newPasswordControl() {
+    return getFormControlAndState(this.form, 'newPassword');
+  }
+
+  get confirmPasswordControl() {
+    return getFormControlAndState(this.form, 'confirmPassword');
   }
 
   submit() {
-    if (this.form.invalid) return;
+    if (
+      this.form.invalid ||
+      !this.form.value.currentPassword ||
+      !this.form.value.newPassword
+    )
+      return;
 
     if (this.form.value.newPassword !== this.form.value.confirmPassword) {
       this.form.get('confirmPassword')?.setErrors({ mismatch: true });
       return;
     }
 
-    this.errorMessage = '';
     this.submitted.emit({
       currentPassword: this.form.value.currentPassword,
       newPassword: this.form.value.newPassword,
