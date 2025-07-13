fa850d6cd741b66ecac2e9ef6c9e40cbd6e7b63e
Jaime Meza <jaimealbertomeza@hotmail.com>
2025-07-13 17:27:07 -0500

    feat: simplify form submission validation logic in InputsShowcasePageComponent

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -4,19 +4,17 @@ import { AuthService } from '../services/auth.service';
 import { LocalStorageService } from '../../shared/services/local-storage.service';
 import { NotificationService } from '../../shared/services/notification.service';
 
-export const authGuard: CanMatchFn = (route, state) => {
+export const authGuard: CanMatchFn = (_route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
   const localStorageService = inject(LocalStorageService);
   const notificationService = inject(NotificationService);
 
   const isAuthenticated = authService.isAuthenticated();
-  console.log('isAuthenticated:', isAuthenticated);
 
   if (isAuthenticated) {
     return true;
   } else {
-    console.log('No está autenticado, redirigiendo a:', `/${state.join('/')}`);
     localStorageService.setItem('redirect_url', `/${state.join('/')}`);
     notificationService.warning(
       'Debes iniciar sesión para acceder a esta sección.',
