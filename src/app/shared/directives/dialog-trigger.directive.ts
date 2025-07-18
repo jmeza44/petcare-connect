9a46c275152754270fbe5005a673f0e10bcf78a4
Jaime Meza <jaimealbertomeza@hotmail.com>
2025-07-17 21:08:25 -0500

    feat: add DialogTriggerDirective for handling dialog interactions

---
+++ src/app/shared/directives/dialog-trigger.directive.ts
@@ -0,0 +1,35 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+  Type,
+  inject,
+} from '@angular/core';
+import { DialogConfig } from '../models/dialog-config.model';
+import { DialogService } from '../services/dialog.service';
+
+@Directive({
+  selector: '[dialogTrigger]',
+  standalone: true,
+  host: {
+    '(click)': 'handleClick()',
+  },
+})
+export class DialogTriggerDirective implements OnInit {
+  private elementRef = inject(ElementRef<HTMLElement>);
+  private dialogService = inject(DialogService);
+
+  @Input() dialogTrigger!: Type<unknown>;
+  @Input() dialogConfig?: DialogConfig;
+
+  ngOnInit(): void {
+    if (!this.dialogTrigger) {
+      throw new Error('dialogTrigger requires a component type');
+    }
+  }
+
+  handleClick(): void {
+    this.dialogService.open(this.dialogTrigger, this.dialogConfig);
+  }
+}
